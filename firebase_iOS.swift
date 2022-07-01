735d8668328186a9f2083895cbac14df2fd3d2ec
hideto1198 <hideto1198@yahoo.co.jp>
2022-07-01 21:26:28 +0900

    initial commit

---
+++ firebase_iOS.swift
@@ -0,0 +1,55 @@
+//
+//  firebase_iOS.swift
+//  BONAFIDE
+//
+//  Created by 東　秀斗 on 2022/07/01.
+//
+
+import Foundation
+import Firebase
+import FirebaseInstanceID
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseMessaging
+
+
+class firebase_test: ObservableObject{
+    @Published var isError: Bool = false
+    @Published var isLoading: Bool = false
+    
+    let db = Firestore.firestore()
+    var functions = Functions.functions()
+    
+    func http_connect(){
+        // 初期化
+        self.isError = false
+        // 通信中のメッセージ or view　を出すためにisLoadingをtrueにしておく
+        self.isLoading = true
+        //返り値を格納する変数 JSONをNSDictionary型に変換する
+        var datas:NSDictionary = NSDictionary()
+        //注意 keyに"data"がないとCloud functionsでデータの受け取りに失敗します。
+        let request: Dictionary<String, Dictionary<String, Any>> = [
+            "data": [
+                "key":"value"
+            ]
+        ]
+        functions.httpsCallable("function_name").call(request){ (result, error) in
+            //エラーハンドリング
+            if error != nil { // errorを使用する場合は if let error = error {　でも良いです。　使わないのに if letを使用すると注意されるので error != nil にしました。
+                self.isLoading = false
+                self.isError = true
+            }else{
+                if let result = result {
+                    datas = result.data as! NSDictionary
+                    /*
+                     受け取ったデータをここで操る
+                     */
+                    self.isLoading = false
+                }else{
+                    self.isLoading = false
+                    self.isError = true
+                }
+            }
+        }
+    }
+}
---
+++ firebase_test_view.swift
@@ -0,0 +1,82 @@
+//
+//  firebase_test_view.swift
+//  BONAFIDE
+//
+//  Created by 東　秀斗 on 2022/07/01.
+//
+
+import SwiftUI
+
+struct firebase_test_view: View {
+    @ObservedObject var Firebase_Test = firebase_test()
+    var body: some View {
+        ZStack{
+            VStack{
+                
+                Button(
+                    action:{
+                        self.Firebase_Test.http_connect()
+                    }
+                ){
+                    Text("Click me")
+                }
+            }
+            .alert(isPresented:self.$Firebase_Test.isError, content:{
+                Alert(
+                    title: Text("エラー"),
+                    message: Text("エラーが発生しました。"),
+                    dismissButton: .default(
+                        Text("OK")
+                    )
+                )
+            })
+        }
+        if self.Firebase_Test.isLoading{
+            loading_view()
+        }
+    }
+}
+
+ 
+struct activity_indicator_view: UIViewRepresentable {
+    public var style = UIActivityIndicatorView.Style.medium
+    
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        return UIActivityIndicatorView(style: style)
+    }
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
+        uiView.startAnimating()
+    }
+}
+    
+struct loading_view: View {
+    var body: some View {
+        ZStack{
+            Color.gray
+                .opacity(0.5)
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Spacer()
+                ZStack{
+                    Color.white
+                        .cornerRadius(15)
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(Color.gray.opacity(0.5), lineWidth: 0.5)
+                    VStack{
+                        activity_indicator_view()
+                        Text("通信中...")
+                    }
+                }.frame(width: width*0.4, height: height*0.15)
+                Spacer()
+            }
+        }
+    }
+}
+
+
+struct firebase_test_view_Previews: PreviewProvider {
+    static var previews: some View {
+        firebase_test_view()
+    }
+}
